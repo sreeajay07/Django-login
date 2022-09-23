a85026537438b3690223c6b483f651d2a7b3f891
sree ajay <86015730+sreeajay07@users.noreply.github.com>
2022-09-23 22:23:35 +0530

    Add files via upload

---
+++ authentication/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+# Create your views here.
+def home(request):
+    return HttpResponse(request, "authentication/index.html")
+
+def signup(request):
+    if request.method == "post":
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        messages.success(request, "Your Account has been successfully created")
+
+        return redirect('signin')
+       
+
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    pass
